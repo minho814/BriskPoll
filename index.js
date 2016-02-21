5744502007c783b915bfab5bca502abd4b26fe24
Min Ho Bae <minho814@gmail.com>
2016-02-21 03:30:21 -0800

    First Commit!

---
+++ index.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var schedule = require('node-schedule');
+
+var userCount = 0;
+var yesVotes = 0;
+var noVotes = 0;
+
+var topicIndex = 0;
+var questions = ["Should abortion be legal?", 
+                 "Is the ACLU good for America?", 
+                 "Can alternative energy effectively replace fossil fuels?", 
+                 "Should animals be used for scientific or commercial testing?", 
+                 "Is sexual orientation determined at birth?", 
+                 "Are cell phones safe?", 
+                 "Should churches remain tax-exempt?", 
+                 "Is human activity a substantial cause of global climate change?", 
+                 "Was Bill Clinton a good president?", 
+                 "Is a college education worth it?", 
+                 "Should adults have the right to carry a concealed handgun?", 
+                 "Does lowering the federal corporate income tax rate create jobs?", 
+                 "Should the United States maintain its embargo against Cuba?", 
+                 "Should the death penalty be allowed?", 
+                 "Should the drinking age be lowered from 21 to a younger age?", 
+                 "Should the United States Continue Its Use of Drone Strikes Abroad?", 
+                 "Should euthanasia or physician-assisted suicide be legal?", 
+                 "Should felons who have completed their sentence be allowed to vote?", 
+                 "Should gay marriage be legal?", 
+                 "Should the United States return to a gold standard?", 
+                 "Is golf a sport?"];
+var topic = questions[topicIndex];
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile('/Users/minho814/Desktop/CompSci/ChatApp/index.html');
+});
+
+// upon user login
+io.on('connection', function(socket){ 
+  console.log('a user connected');
+  userCount = userCount + 1;
+  io.emit('userUpdate', userCount); // update user count
+  socket.emit('voteUpdate', yesVotes, noVotes);
+  socket.emit('topicUpdate', topic)
+
+  // upon user disconnect
+  socket.on('disconnect', function(){ 
+    console.log('user disconnected'); 
+    userCount = userCount - 1;
+    io.emit('userUpdate', userCount); // update user count
+  });
+
+  // upon red button press
+  socket.on('red press', function(msg, vote, change){
+    noVotes = noVotes + vote;
+    io.emit('voteUpdate', yesVotes, noVotes)
+    if (vote > 0) {
+      console.log('message: ' + msg);
+      io.emit('red press', msg, 'all', change);
+      socket.emit('red press', msg);
+      socket.emit('positionUpdate', 'No');
+    }
+  });
+
+  // upon blue button press
+  socket.on('blue press', function(msg, vote, change){ 
+    yesVotes = yesVotes + vote;
+    io.emit('voteUpdate', yesVotes, noVotes)
+    if (vote > 0) {
+      console.log('message: ' + msg);
+      io.emit('blue press', msg, 'all', change);
+      socket.emit('blue press', msg);
+      socket.emit('positionUpdate', 'Yes');
+    }
+  });
+
+  var j = schedule.scheduleJob({hour: 00, minute: 00}, function(){
+    topicIndex = (topicIndex + 1) % questions.length;
+    topic = questions[topicIndex];
+    yesVotes = 0;
+    noVotes = 0;
+    io.emit('reset', topic);
+  });
+});
+
+
+http.listen(process.env.PORT, function(){
+  //console.log('listening on *:3000');
+});
---
+++ public/js/script.js
@@ -0,0 +1,96 @@
+var socket = io();
+var change = false;
+
+$(document).ready(function() {
+
+  // Listen for user click of "No"
+  $('.red').on('click', function(){
+    change = false;
+    if ($('.blue').is(':disabled')) {
+      change = true;
+      socket.emit('blue press', $('#m').val(), -1);
+    }
+    socket.emit('red press', $('#m').val(), 1, change);
+    $('#m').val('');
+    return false;
+  });
+
+  // Listen for user click of "Yes"
+  $('.blue').on('click', function(){
+    change = false;
+    if ($('.red').is(':disabled')) {
+      change = true;
+      socket.emit('red press', $('#m').val(), -1);
+    }
+    socket.emit('blue press', $('#m').val(), 1, change);
+    $('#m').val('');
+    return false;
+  });
+
+  // upon user connection
+  socket.on('voteUpdate', function(yes, no){
+    $('#yesvote').text(yes); // update votes
+    $('#novote').text(no);
+  });
+
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
+
+  // Response to user click of "No"
+  socket.on('red press', function(msg, who, changeStatus){
+    if (who == 'all') {
+      if (changeStatus) {
+        $('#messages').append($('<li class = "redvote">').text('Change from Yes to No: ' + msg));
+      }
+      else {
+        $('#messages').append($('<li class = "redvote">').text('Vote for No: ' + msg));
+      }
+    }
+    else {
+      $('.red').prop('disabled', true);
+      $('.blue').prop('disabled', false);
+    }
+  });
+
+  // Response to user click of "Yes"
+  socket.on('blue press', function(msg, who){
+    if (who == 'all') {
+      if (change) {
+        $('#messages').append($('<li class = "bluevote">').text('Change from No to Yes: ' + msg));
+      }
+      else {
+        $('#messages').append($('<li class = "bluevote">').text('Vote for Yes: ' + msg));
+      }
+    }
+    else {
+      $('.blue').prop('disabled', true);
+      $('.red').prop('disabled', false);
+    }
+  });
+
+  // Update online user count
+  socket.on('userUpdate', function(userCount) {
+    $('#userCount').text(userCount);
+  });
+
+  // Update your position
+  socket.on('positionUpdate', function(position) {
+    $('#vote').text(position);
+  });
+
+  socket.on('topicUpdate', function(topicQuestion){
+    $('#topic').text(topicQuestion);
+  });
+
+  // Reset the screen
+  socket.on('reset', function(topicQuestion) {
+    $('#messages').empty(); // empty chat
+    $('#yesvote').text(0); // update votes
+    $('#vote').text('');
+    $('#novote').text(0);
+    $('.blue').prop('disabled', false); // reset buttons
+    $('.red').prop('disabled', false);
+    $('#topic').text(topicQuestion);
+  });
+})
